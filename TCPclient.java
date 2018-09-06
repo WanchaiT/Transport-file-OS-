4c31ee9d00c3c9d56a63175aa2d9ad41e0a3958c
Wanchai Thiantanawat <wanchairoot@WanMac.local>
2018-09-06 11:45:16 +0700

    fin

--- TCPclient.java
+++ TCPclient.java
@@ -1,24 +1,32 @@
 import java.io.*;
 import java.net.*;
 import java.util.*;
-import java.util.Timer;
 class TCPClient {
     public static void main(String argv[]) throws Exception{
         long a = System.currentTimeMillis();
 
-        String sentence;
-        String modifiedSentence;
-        Scanner inFromUser = new Scanner(System.in);
+        String path = "/Users/wanchairoot/Documents/517312 Operating Systems/Gifted.mp4";
+        InputStream file = new FileInputStream(path);
+        byte[] bytes = new byte[1000000000];
+
         Socket clientSocket = new Socket("localhost", 49152);
         DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
-        Scanner inFromServer = new Scanner (clientSocket.getInputStream());
-        System.out.print("Please enter words: ");
-        sentence = inFromUser.nextLine();
-        outToServer.writeBytes(sentence + '\n');
-        modifiedSentence = inFromServer.nextLine();
-        System.out.println("FROM SERVER: " + modifiedSentence);
+        //Scanner inFromServer = new Scanner (clientSocket.getInputStream());
+        int index;
+        /*while ((index = file.read(bytes)) != -1) {
+            outToServer.write(bytes ,0 ,index);
+        }*/
+        outToServer.writeBytes(path);
+
+        System.out.println("out");
+        //modifiedSentence = inFromServer.nextLine();
+
+        //System.out.println("FROM SERVER: " + modifiedSentence);
         clientSocket.close();
 
         System.out.println(System.currentTimeMillis() - a);
     }
  }
+
+ //http://na5cent.blogspot.com/2015/01/java-stream.html
+ //http://computingblog.intakosum.net/2013/12/socket-4-tcp-client-java.html?m=0
--- TCPserver.java
+++ TCPserver.java
@@ -3,17 +3,28 @@ import java.net.*;
 import java.util.*;
 class TCPServer {
     public static void main(String argv[]) throws Exception{
-        String clientSentence;
-        String capitalizedSentence;
+        InputStream fileInput = null;
+        OutputStream fileOutput = null;
         ServerSocket welcomeSocket = new ServerSocket(49152);
         while(true) {
+            long a = 0;
             System.out.println("The server is waiting ");
             Socket connectionSocket = welcomeSocket.accept();
             Scanner inFromClient = new Scanner(connectionSocket.getInputStream());
-            DataOutputStream  outToClient = new DataOutputStream(connectionSocket.getOutputStream());
-            clientSentence = inFromClient.nextLine();
-            capitalizedSentence = clientSentence.toUpperCase() + '\n';
-            outToClient.writeBytes(capitalizedSentence);
+            //DataOutputStream  outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+            fileInput = new FileInputStream(inFromClient.nextLine());
+            a = System.currentTimeMillis();
+            fileOutput = new FileOutputStream("/Users/wanchairoot/Documents/517312 Operating Systems/fileFromServer/Movie.mp4");
+
+            byte[] bytes = new byte[1000000000];
+            int index;
+
+            while ((index = fileInput.read(bytes)) != -1) {
+                fileOutput.write(bytes, 0, index);
+            }
+            System.out.println((System.currentTimeMillis() - a) / 1000 + " second");
+            /*capitalizedSentence = clientSentence.toUpperCase() + '\n';
+            outToClient.writeBytes(capitalizedSentence);*/
         }
-    }
-}
+      }
+  }
