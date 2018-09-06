763113670cfd9cacb2574bec585f8b03d5c7ce15
Wanchai Thiantanawat <wanchairoot@WanMac.local>
2018-09-06 15:31:55 +0700

    remove timestop server

--- TCPserver.java
+++ TCPserver.java
@@ -7,21 +7,18 @@ class TCPServer {
         OutputStream fileOutput = null;
         ServerSocket welcomeSocket = new ServerSocket(49152);
         while(true) {
-            long a = 0;
             System.out.println("The server is waiting ");
             Socket connectionSocket = welcomeSocket.accept();
             Scanner inFromClient = new Scanner(connectionSocket.getInputStream());
             fileInput = connectionSocket.getInputStream();
-
-            a = System.currentTimeMillis();
+            System.out.println(fileInput);
             fileOutput = new FileOutputStream("/Users/wanchairoot/Documents/517312 Operating Systems/fileFromServer/a.zip");
-
             byte[] bytes = new byte[1000000000];
             int index;
             while ((index = fileInput.read(bytes)) != -1) {
                 fileOutput.write(bytes, 0 ,index);
             }
-            System.out.printf("%.2f second " ,(System.currentTimeMillis() - a)/1000.00 );
+            System.out.println("Finish");
         }
     }
 }
